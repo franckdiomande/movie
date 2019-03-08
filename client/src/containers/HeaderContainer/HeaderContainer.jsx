79b8a60a958f70866887804fecb08407d782888a
Unknown <nsbx@hotmail.fr>
2019-03-08 16:11:28 +0100

    add logout

--- client/src/containers/HeaderContainer/HeaderContainer.jsx
+++ client/src/containers/HeaderContainer/HeaderContainer.jsx
@@ -1,30 +1,49 @@
-import './header-container.scss';
-import React from 'react';
+import "./header-container.scss";
+import React from "react";
 import SearchComponent from "../../components/SearchComponent/SearchComponent.jsx";
-import {Route, Switch} from "react-router-dom";
-import {withRouter} from "react-router";
-import {connect} from "react-redux";
-import {LoginFormContainer} from "../LoginFormContainer/LoginFormContainer";
+import { Route, Switch } from "react-router-dom";
+import { withRouter } from "react-router";
+import { connect } from "react-redux";
+import { LoginFormContainer } from "../LoginFormContainer/LoginFormContainer";
 
 class HeaderContainer extends React.Component {
     render() {
         return (
-            <div id={'header-container'}>
+            <div id={"header-container"}>
+                <img
+                    src="../images/logo.svg"
+                    alt="Movie app"
+                    id={"logo"}
+                    onClick={() => {
+                        this.props.history.push("/");
+                    }}
+                />
 
-                <img src="../images/logo.svg" alt="Movie app" id={'logo'} onClick={()=>{this.props.history.push('/')}}/>
+                <img
+                    src="../images/video-player.svg"
+                    alt="Video player"
+                    id={"add-movie"}
+                    onClick={() => {
+                        this.props.history.push("/movies/add");
+                    }}
+                />
 
-                <img src="../images/video-player.svg" alt="Video player" id={'add-movie'} onClick={()=>{this.props.history.push('/movies/add')}}/>
-
-                <img src="../images/user.png" alt="Movie app" id={'profile'}/>
-
+                <img
+                    src="../images/exit.png"
+                    alt="Logout"
+                    id={"logout"}
+                    onClick={() => {
+                        window.localStorage.removeItem("token");
+                        this.props.history.push("/login");
+                    }}
+                />
             </div>
-        )
+        );
     }
 }
 
-
 function mapStateToProps(state) {
-    return {}
+    return {};
 }
 
 export default withRouter(connect(mapStateToProps)(HeaderContainer));
